39bd175a163e2e309733f4f661a360289a82886b
@randomSveta <sveta_klimova@outlook.com>
2024-03-31 21:41:19 +0100

    fix: move response before executeFlow

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -6,8 +6,8 @@ import { flows } from "../flows/index";
 const eventController = async (req: Request, res: Response): Promise<void> => {
   try {
     const event: Event = req.body;
+    res.status(200).send("Request received. Executing flow...");
     await executeFlow(event, flows);
-    res.status(200).send("Flow executed successfully.");
   } catch (error) {
     console.error("Error executing flow:", error);
     res.status(500).send("Internal server error.");
